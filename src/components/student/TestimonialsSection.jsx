7b98adae17b9d40c8dfdcc03d46fe60c08be4b13
Aryan Patel <aryanpatel6215@gmail.com>
2025-07-21 21:15:16 +0530

    feat: added testimonials

--- src/components/student/TestimonialsSection.jsx
+++ src/components/student/TestimonialsSection.jsx
@@ -1,9 +1,15 @@
+import { assets, dummyTestimonial, testimonials } from '../../assets/assets'
 import React from 'react'
+import { AnimatedTestimonials } from '../ui/animated-testimonials'
 
-const TestimonialsSection = () => {
+const TestimonialSection = () => {
   return (
-    <div>TestimonialsSection</div>
+    <div className='pb-14 px-8 md:px-0'>
+      <h2 className='text-3xl font-bold text-gray-800'>Testimonials</h2>
+      <p className='md:text-base text-gray-500 mt-3'>Hear from our learners as they share their journeys of transformations, success, and how our <br /> platform has made a difference in their lives.</p>
+      <AnimatedTestimonials testimonials={testimonials} autoplay={true}/>
+    </div>
   )
 }
 
-export default TestimonialsSection
+export default TestimonialSection
