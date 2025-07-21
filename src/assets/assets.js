98564034a95d70ab4e53e1e1f2d9d430fefa2162
Aryan Patel <aryanpatel6215@gmail.com>
2025-07-21 19:45:36 +0530

    update: added dummy testimonials

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -125,6 +125,44 @@ export const dummyTestimonial = [
     },
 ];
 
+export const testimonials = [
+    {
+      quote:
+        'I\'ve been using Learnify for nearly two years, primarily for Instagram, and it has been incredibly user-friendly, making my work much easier.',
+      name: "Sarah Chen",
+      designation: "SWE 1 @ Amazon",
+      src: "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?q=80&w=3560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      quote:
+        "Learnify has been my go-to tool for over a year now—especially for Instagram—and its simplicity has saved me so much time and effort.",
+      name: "Michael Rodriguez",
+      designation: "CTO at InnovateSphere",
+      src: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      quote:
+        "Learnify’s user-friendly design is what keeps me coming back. It’s been a reliable companion in my Instagram journey for nearly two years.",
+      name: "Emily Watson",
+      designation: "Operations Director at CloudScale",
+      src: "https://images.unsplash.com/photo-1623582854588-d60de57fa33f?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      quote:
+        "Outstanding support and robust features. It's rare to find a product that delivers on all its promises.",
+      name: "James Kim",
+      designation: "SWE 2 @ Google",
+      src: "https://images.unsplash.com/photo-1636041293178-808a6762ab39?q=80&w=3464&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      quote:
+        "Learnify has been my go-to tool for over a year now—especially for Instagram—and its simplicity has saved me so much time and effort.",
+      name: "Lisa Thompson",
+      designation: "VP of Technology at FutureNet",
+      src: "https://images.unsplash.com/photo-1624561172888-ac93c696e10c?q=80&w=2592&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+  ];
+
 export const dummyDashboardData = {
     "totalEarnings": 707.38,
     "enrolledStudentsData": [
