7a8128a7e1414de9cab7c2e42d6a823ed7a7b28b
Aryan Patel <aryanpatel6215@gmail.com>
2025-07-22 13:22:37 +0530

    feat: added Course Details & duration functions

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,6 +1,7 @@
 import { useState, createContext, useEffect } from 'react'
 import { dummyCourses } from '../assets/assets'
 import { useNavigate } from 'react-router-dom'
+import humanizeDuration from 'humanize-duration'
 
 export const AppContext = createContext()
 
@@ -29,12 +30,40 @@ export const AppContextProvider = (props) => {
 
     }
 
+    // function to calculate course chapter time
+    const calculateChapterTime = (chapter)=>{
+        let time = 0
+        chapter.chapterContent.map((lecture)=> time += lecture.lectureDuration)
+        // convert into hrs and min
+        return humanizeDuration(time*60*1000,{units:["h","m"]})
+    }
+
+    // function to calculate the total course duration 
+    const calculateCourseDuration = ()=>{
+        let time = 0 
+        course.courseContent.map((chapter)=>chapter.chapterContent.map((lecture)=> time += lecture.lectureDuration))
+        return humanizeDuration(time*60*1000,{units:["h","m"]})
+    }
+
+    // function to calculate the total no in the course
+    const calculateNoOfLectures = (course)=>{
+        let totalLectures = 0
+        course.courseContent.forEach((chapter)=>{
+            if( Array.isArray(chapter.chapterContent)){
+                totalLectures += chapter.chapterContent.length
+            }
+        });
+        return totalLectures
+    }
+
+
+
     useEffect(() => {
         fetchAllCourses()
     }, [])
 
     const value = {
-        currency,allCourses,navigate,calculateRating,isEducator,setIsEducator
+        currency,allCourses,navigate,calculateRating,isEducator,setIsEducator,calculateChapterTime,calculateCourseDuration,calculateNoOfLectures
         
     }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,13 +7,16 @@ export default {
     theme: {
       extend: {
         // customizations
-        // fontSize:{
-        //     'course-details-heading-small':['26px','36px'],
-        //     'course-details-heading-large':['36px','44px'],
-        //     'home-heading-small':['28px','34px'],
-        //     'home-heading-large':['48px','56px'],
-        //     'default':['15px','21px']      
-        // }
+        fontSize:{
+            'course-details-heading-small':['26px','36px'],
+            'course-details-heading-large':['36px','44px'],
+            'home-heading-small':['28px','34px'],
+            'home-heading-large':['48px','56px'],
+            'default':['15px','21px']      
+        },
+        spacing:{
+            'section-height': '500px',
+        }
       },
     },
     plugins: [],
